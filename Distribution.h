c9c2c2236389b439511f8fc01623885a134d7cc6
zenith-grift <josh.frandsen3@gmail.com>
2021-08-04 11:09:39 -0600

    added files for distribution class

---
+++ Distribution.h
@@ -0,0 +1,17 @@
+//
+// Created by Josh Frandsen on 7/27/21.
+//
+
+#ifndef PROJECT2_JFRANDSEN_DISTRIBUTION_H
+#define PROJECT2_JFRANDSEN_DISTRIBUTION_H
+
+#include <iostream>
+
+using namespace std;
+
+class Distribution {
+private:
+public:
+};
+
+#endif // PROJECT2_JFRANDSEN_DISTRIBUTION_H
